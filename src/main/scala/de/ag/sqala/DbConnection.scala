694461a771924639073d8983607f8fcd64d1ebf4
Andreas Bernauer <andreas.bernauer@active-group.de>
2013-07-18 14:12:16 +0200

    Refine DbConnection interface

--- src/main/scala/de/ag/sqala/DbConnection.scala
+++ src/main/scala/de/ag/sqala/DbConnection.scala
@@ -5,6 +5,37 @@ class Cursor //FIXME
 sealed abstract class Handle
 case class JDBCHandle(connection: java.sql.Connection) extends Handle
 
+class ResultSetIterator(resultSet:java.sql.ResultSet) extends Iterator[Seq[AnyRef]] {
+  private val metaData = resultSet.getMetaData
+  private val columnCount = metaData.getColumnCount
+  private var needNext = true
+  private var depleted = false
+  def next():Seq[AnyRef] = {
+    maybeAdvanceResultSetCursor()
+    needNext = true
+    (1 to columnCount).map(resultSet.getObject)
+  }
+
+  def hasNext = {
+    maybeAdvanceResultSetCursor()
+    depleted
+  }
+
+  /* ResultSet API only tells me whether there is a next by actually
+     calling next().  This contrasts Iterator's hasNext, which can be
+     called many times.  So we keep track whether we've called next and
+     whether we're at the end of the set.
+  */
+  private def maybeAdvanceResultSetCursor() {
+    if (needNext) {
+      depleted = resultSet.next()
+      needNext = false
+    }
+  }
+
+}
+
+
 /**
  *
  */
@@ -15,8 +46,8 @@ trait DbConnection {
   val sqlWriteParameterization: SqlWriteParameterization
 
   def close():Unit
-  def query(sqlQuery:SqlQuery, schema:Schema): Cursor
-  def insert(table:SqlTableName, schema:Schema, values:Seq[Any/*FIXME*/]): Any /*FIXME*/ // FIXME structured?
+  def query(sqlQuery:SqlQuery, schema:Schema): ResultSetIterator
+  def insert(table:SqlTableName, schema:Schema, values:Seq[AnyRef/*FIXME*/]): Int /*FIXME*/ // FIXME structured?
   def delete(table:SqlTableName, expr:SqlExpr): Any /*FIXME*/ // FIXME structured?
   def update(table:SqlTableName, schema:Schema, expr:SqlExpr, a:Seq[(String, SqlExpr)]): Int // FIXME structured?
   def execute(sql:String):Any /*FIXME*/ // run raw SQL, for emergencies
