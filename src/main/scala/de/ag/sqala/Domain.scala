18c3a50ef8fdfd2862b9e8a0bd622de9afc248f0
Andreas Bernauer <andreas.bernauer@active-group.de>
2013-07-19 13:40:52 +0200

    More domains

--- src/main/scala/de/ag/sqala/Domain.scala
+++ src/main/scala/de/ag/sqala/Domain.scala
@@ -3,10 +3,25 @@ package de.ag.sqala
 /**
  *
  */
-abstract class Domain(val name: String)
+abstract class Domain(val name: String) {
+  def isNumeric: Boolean = this match {
+    case DBInteger | DBDouble => true
+    case _ => false
+  }
+
+  def isOrdered: Boolean = this match { // what about product, null, bounded string, blob?
+    case DBInteger | DBDouble | DBString | DBCalendarTime => true
+    case _ => false
+  }
+}
 case object DBString extends Domain("string")
 case object DBInteger extends Domain("integer")
 case object DBDouble extends Domain("double")
 case object DBBoolean extends Domain("boolean")
 case object DBCalendarTime extends Domain("calendar time")
 case object DBBlob extends Domain("blob")
+case class DBBoundedString(maxSize:Int) extends Domain("bounded string")
+case class DBNullable(underlying:Domain) extends Domain("nullable '" + underlying.name + "'")
+case class DBProduct(components:Seq[Domain]) extends Domain("product of '" + components.map{_.name} + "'")
+case class DBSet(member:Domain) extends Domain("set of '" + member.name + "'")
+
