c24a6007d23384a4eff6c72556ac95597bdfb1b9
Andreas Bernauer <andreas.bernauer@active-group.de>
2013-07-24 18:13:32 +0200

    Add Db2DbConnection + tests

---
+++ sqala-testkit/src/test/scala/de/ag/sqala/test/Db2Test.scala
@@ -0,0 +1,109 @@
+package de.ag.sqala.test
+
+import org.scalatest.{BeforeAndAfter, FunSuite}
+import de.ag.sqala.drivers.Db2DbConnection
+import de.ag.sqala.sql._
+import de.ag.sqala.{ResultSetIterator, Domain, DbConnection, Operator}
+import de.ag.sqala.relational.Schema
+
+/**
+ *
+ */
+class Db2Test extends FunSuite with BeforeAndAfter {
+  var conn:DbConnection = _
+  val where = new Db2DbConnection.Location("192.168.1.138", 50001, "test")
+
+  before {
+    conn = Db2DbConnection.open(where, "db2inst2", "db2inst2")
+  }
+
+  val tbl1Schema: Schema = new Schema(Seq(("one", Domain.String), ("two", Domain.Integer)))
+
+  val data:Seq[(String, java.lang.Integer)] = Seq(
+    ("test", 10),
+    ("foo", 12),
+    ("bar", -1),
+    ("filderstadt", 70794)
+  )
+
+  def createTbl1() {
+    // DROP TABLE IF EXISTS
+    conn.execute("select tabname from syscat.tables where tabschema='DB2INST2' and tabname='TBL1'") match {
+      case Left(it:ResultSetIterator) =>
+        if (it.size > 0)
+          conn.execute("DROP TABLE tbl1")
+      case Right(count) => throw new RuntimeException("unexpectedly received update count")
+    }
+    conn.execute("CREATE TABLE tbl1(one VARCHAR(11), two INT)")
+  }
+
+  def createAndFillTbl1() {
+    createTbl1()
+    data.foreach {
+      case (w, s) =>
+        conn.insert("tbl1", tbl1Schema, Seq(w, Int.box(s)))
+    }
+  }
+
+  test("open/close") {}
+
+  test("create tables") {
+    createTbl1()
+  }
+
+  test("insert") {
+    createTbl1()
+    expectResult(1){conn.insert("tbl1", tbl1Schema, Seq("test", Int.box(10)))}
+  }
+
+  test("insert & query") {
+    createTbl1()
+    assert(1 == conn.insert("tbl1", tbl1Schema, Seq("test", Int.box(10))))
+
+    val results = conn.query(Query.makeSelect(
+      attributes = Seq(QuerySelectAttribute(ExprColumn("one"), None), QuerySelectAttribute(ExprColumn("two"), None)),
+      from = Seq(QuerySelectFromQuery(QueryTable("tbl1"), None))
+    ),
+      new Schema(Seq(("one", Domain.String), ("two", Domain.Integer))))
+      .toArray
+
+    expectResult(1) {
+      results.size
+    }
+    expectResult(Seq("test", 10)) {
+      results(0)
+    }
+  }
+
+  test("insert & query many") {
+    createAndFillTbl1()
+    expectResult(data.map{d => Seq(d._1, d._2)}.toSet){
+      conn.query(QueryTable("tbl1"), tbl1Schema)
+        .toSet
+    }
+  }
+
+  test("delete") {
+    createAndFillTbl1()
+
+    expectResult(1){conn.delete("tbl1", ExprApp(Operator.Eq, Seq(ExprColumn("one"), ExprConst(LiteralString("test")))))}
+    expectResult(2){conn.delete("tbl1", ExprApp(Operator.Or, Seq(
+      ExprApp(Operator.Eq, Seq(ExprColumn("one"), ExprConst(LiteralString("foo")))),
+      ExprApp(Operator.Eq, Seq(ExprColumn("two"), ExprConst(LiteralInteger(-1)))))))}
+    expectResult(0){conn.delete("tbl1", ExprApp(Operator.Eq, Seq(ExprColumn("one"), ExprConst(LiteralString("test")))))}
+  }
+
+  test("update") {
+    createAndFillTbl1()
+
+    expectResult(1){conn.update("tbl1",
+      ExprApp(Operator.Eq, Seq(ExprColumn("one"), ExprConst(LiteralString("bar")))),
+      Seq(("two", ExprConst(LiteralInteger(12)))))}
+    expectResult(0){conn.update("tbl1",
+      ExprApp(Operator.Eq, Seq(ExprColumn("one"), ExprConst(LiteralString("not there")))),
+      Seq(("two", ExprConst(LiteralInteger(12)))))}
+    expectResult(2){conn.update("tbl1",
+      ExprApp(Operator.Eq, Seq(ExprColumn("two"), ExprConst(LiteralInteger(12)))),
+      Seq(("two", ExprConst(LiteralNull))))}
+  }
+}
---
+++ src/main/scala/de/ag/sqala/drivers/Db2DbConnection.scala
@@ -0,0 +1,156 @@
+package de.ag.sqala.drivers
+
+import de.ag.sqala.sql._
+import java.io.{File, Writer}
+import java.sql.Date
+import java.util.Properties
+import de.ag.sqala._
+import de.ag.sqala.JDBCHandle
+import de.ag.sqala.sql.LiteralBoolean
+import de.ag.sqala.sql.QueryCombine
+import de.ag.sqala.relational.Schema
+import java.net.URL
+
+/**
+ * Driver for DB2 database
+ */
+class Db2DbConnection(connection:java.sql.Connection) extends DbConnection {
+  val kind: Symbol = 'db2
+  val name: String = "DB2"
+  val handle: Handle = JDBCHandle(connection)
+  val sqlWriteParameterization: WriteParameterization = new WriteParameterization {
+    /**
+     * write constant SQL expression (literal) to output sink
+     *
+     * @param out: output sink
+     * @param value: constant literal to write
+     */
+    def writeLiteral(out: Writer, value: Literal) {
+      value match {
+        case LiteralBoolean(b) => out.write(if (b) "1" else "0")
+        case _ => defaultSqlWriteParameterization.writeLiteral(out, value)
+      }
+    }
+
+    /**
+     * write QueryCombine to output sink
+     *
+     * @param out output sink
+     * @param param this object for recursive calls
+     * @param sqlCombine left sql query, combine operator, right sql query
+     */
+    def writeCombine(out: Writer, param: WriteParameterization, sqlCombine: QueryCombine) {
+      out.write("SELECT * FROM (")
+      sqlCombine.left.write(out, param)
+      sqlCombine.op match {
+        case CombineOpIntersect => " INTERSECT "
+        case CombineOpExcept => " EXCEPT "
+        case CombineOpUnion => " UNION "
+      }
+      sqlCombine.right.write(out, param)
+      out.write(")")
+    }
+  }
+
+  def close() {
+    connection.close()
+  }
+
+  def query(sqlQuery: Query, schema: Schema): ResultSetIterator = {
+    val sql = sqlQuery.toString(sqlWriteParameterization)
+    val statement = connection.createStatement()
+    val resultSet = statement.executeQuery(sql)
+    // first shot: primitively return Objects
+    new ResultSetIterator(resultSet)
+  }
+
+
+  def insert(tableName: TableName, schema: Schema, values: Seq[AnyRef]): Int = {
+    val sql = "INSERT INTO %s(%s) VALUES (%s)".format(
+      tableName,
+      schema.attributes.mkString(", "),
+      listToPlaceholders(values).mkString(", ")
+    )
+    val statement = connection.prepareStatement(sql)
+    values
+      .zip(schema.domains)
+      .zip(1 to schema.degree) // parameter count starts with 1
+      .foreach {
+      case ((value, domain), i) =>
+        domain match {
+          case Domain.String => statement.setString(i, value.asInstanceOf[String])
+          case Domain.Integer => statement.setInt(i, value.asInstanceOf[Integer].intValue())
+          case Domain.Double => statement.setDouble(i, value.asInstanceOf[Double])
+          case Domain.Boolean => statement.setBoolean(i, value.asInstanceOf[Boolean])
+          case Domain.CalendarTime => statement.setDate(i, value.asInstanceOf[Date])
+          case Domain.Blob => statement.setBlob(i, value.asInstanceOf[java.io.InputStream])
+          case _ => throw new RuntimeException("unknown domain " + domain)
+        }
+    }
+    val result = statement.executeUpdate()
+    statement.close()
+    result
+  }
+
+
+  private def listToPlaceholders(values: Seq[Any]): Seq[String] = {
+    values.map{_=>"?"}
+  }
+
+  def delete(tableName: TableName, condition: Expr): Int = {
+    val sql = "DELETE FROM %s WHERE %s".format(tableName, condition.toString(sqlWriteParameterization))
+    val statement = connection.createStatement()
+    val result = statement.executeUpdate(sql)
+    statement.close()
+    result
+  }
+
+
+  def update(tableName: TableName, condition: Expr, updates: Seq[(ColumnName, Expr)]): Int = {
+    val clauses = updates.map {
+      case (columnName, value) => "%s = %s".format(columnName, value.toString(sqlWriteParameterization))
+    }
+    val sql = "UPDATE %s SET %s WHERE %s".format(
+      tableName,
+      clauses.mkString(", "),
+      condition.toString(sqlWriteParameterization)
+    )
+    val statement = connection.createStatement()
+    val result = statement.executeUpdate(sql)
+    statement.close()
+    result
+  }
+
+  def execute(sql: String): Either[ResultSetIterator, Int] = {
+    val statement = connection.createStatement()
+    if (statement.execute(sql))
+      Left(new ResultSetIterator(statement.getResultSet))
+    else
+      Right(statement.getUpdateCount)
+  }
+}
+
+object Db2DbConnection {
+  class Location(val host:String, val port:Int, val database:String) {
+    if (!(port > 0 && port <= 65535))
+      throw new IllegalArgumentException("port number must be positive and below 65535, not " + port)
+  }
+
+  def open(db2Loc:Location, user:String, password:String): Db2DbConnection = {
+    val props = new Properties()
+    props.setProperty("user", user)
+    props.setProperty("password", password)
+    open(db2Loc, props)
+  }
+
+  def open(db2Loc:Location, properties:Properties): Db2DbConnection = {
+    val where = "//%s:%s/%s".format(db2Loc.host, db2Loc.port, db2Loc.database)
+    open(where, properties)
+  }
+
+  private def open(where: String, properties: Properties): Db2DbConnection = {
+    Class.forName("com.ibm.db2.jcc.DB2Driver")
+    val connection = java.sql.DriverManager.getConnection("jdbc:db2:" + where, properties)
+    new Db2DbConnection(connection)
+  }
+}
