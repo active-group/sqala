1c836a09450109c01cf597f750cc038aa6afff2f
Andreas Bernauer <andreas.bernauer@active-group.de>
2013-07-24 18:17:23 +0200

    TestDb2Driver.java -> Db2JDBCTest.scala

---
+++ sqala-testkit/src/test/scala/de/ag/sqala/test/Db2JDBCTest.scala
@@ -0,0 +1,56 @@
+package de.ag.sqala.test
+
+import java.math.BigDecimal
+import java.sql._
+
+/**
+ *
+ */
+object Db2JDBCTest extends App {
+  System.out.println("Hello world!")
+  testDb2Sample("192.168.1.138", "50001", "db2inst2", "db2inst2")
+
+  def testDb2Sample(host: String, port: String, user: String, pass: String) {
+    val database: String = "sample"
+    var connection: Connection = null
+    try {
+      Class.forName("com.ibm.db2.jcc.DB2Driver")
+      connection = DriverManager.getConnection("jdbc:db2://" + host + ":" + port + "/" + database, user, pass)
+      val preparedStatement: PreparedStatement = connection.prepareStatement("SELECT * FROM STAFF WHERE DEPT = ?")
+      preparedStatement.setInt(1, 20)
+      val resultSet: ResultSet = preparedStatement.executeQuery
+      val strFormat: String = "%-5s %-12s %-5s %-5s %-5s %-10s %-10s"
+      val header: String = String.format(strFormat, "id", "name", "dept", "job", "years", "salary", "comm")
+      System.out.println(header)
+      System.out.println(String.format(strFormat, "-----", "------------", "-----", "-----", "-----", "----------", "----------"))
+      while (resultSet.next) {
+        val id: Int = resultSet.getInt("ID")
+        val name: String = resultSet.getString("NAME")
+        val dept: Int = resultSet.getInt("DEPT")
+        val job: String = resultSet.getString("JOB")
+        val years: Int = resultSet.getInt("YEARS")
+        val salary: BigDecimal = resultSet.getBigDecimal("SALARY")
+        val comm: BigDecimal = resultSet.getBigDecimal("COMM")
+        val out: String = "%5d %-12s %5d %5s %5d %10.2f %10.2f".format(id, name, dept, job, years, salary, comm)
+        System.out.println(out)
+      }
+    }
+    catch {
+      case e: Exception => {
+        e.printStackTrace()
+      }
+    }
+    finally {
+      if (connection != null) {
+        try {
+          connection.close()
+        }
+        catch {
+          case e: SQLException => {
+            e.printStackTrace()
+          }
+        }
+      }
+    }
+  }
+}
