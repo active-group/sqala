093faab17891f6eea7d59e3a454dfb74057a41ca
Andreas Bernauer <andreas.bernauer@active-group.de>
2013-07-25 12:05:07 +0200

    Document relational.Expr

--- src/main/scala/de/ag/sqala/relational/Expr.scala
+++ src/main/scala/de/ag/sqala/relational/Expr.scala
@@ -3,9 +3,13 @@ package de.ag.sqala.relational
 import de.ag.sqala.{AggregationOperator, Operator, Domain}
 
 /**
- *
+ * Expression in a relational algebra
  */
 sealed abstract class Expr {
+  /**
+   * Check if expression contains Expr.Aggregation
+   * @return
+   */
   def isAggregate:Boolean = this match {
     case _:Expr.AttributeRef
          | _:Expr.Const
@@ -21,7 +25,24 @@ sealed abstract class Expr {
         default.exists(_.isAggregate)
   }
 
-  /*
+  /**
+   * (Right-)Fold over an expression (catamorphism).
+   *
+   * This implementation currently uses recursion.
+   *
+   * @param onAttributeRef   fold Expr.AttributeRef
+   * @param onConst          fold Expr.Const
+   * @param onNull           fold Expr.Null
+   * @param onApplication    fold Expr.Application
+   * @param onTuple          fold Expr.Tuple
+   * @param onAggregation    fold Expr.Aggregation
+   * @param onCase           fold Expr.Case
+   * @param onScalarSubQuery fold Expr.ScalarSubQuery
+   * @param onSetSubQuery    fold Expr.SetSubQuery
+   * @tparam T               type of result
+   * @return                 right-fold of expression
+   */
+  /* Copy this to help calling
       onAttributeRef= {name => ???},
       onConst= {value => ???},
       onNull= {domain => ???},
@@ -57,15 +78,25 @@ sealed abstract class Expr {
 }
 
 object Expr {
+  /** attribute reference */
   case class AttributeRef(name:String) extends Expr
+  /** constant value */
   case class Const(domain:Domain, value:String /*FIXME?*/) extends Expr
+  /** a nullable domain */
   case class Null(typ:Domain) extends Expr
+  /** application (but not with aggregate op FIXME) */
   case class Application(operator:Operator, operands:Seq[Expr]) extends Expr
+  /** tuples */
   case class Tuple(expressions:Seq[Expr]) extends Expr
+  /** application with aggregation operator (count, sum, etc.) */
   case class Aggregation(op:AggregationOperator, expr:Expr) extends Expr
+  /** case ('switch') */
   case class Case(branches:Seq[CaseBranch], default:Option[Expr]) extends Expr
+  /** sub-query returning a scalar */
   case class ScalarSubQuery(query:Query) extends Expr
+  /** sub-query returning a set */
   case class SetSubQuery(query:Query) extends Expr
 
+  /** branch of a case */
   case class CaseBranch(condition:Expr, value:Expr)
 }
