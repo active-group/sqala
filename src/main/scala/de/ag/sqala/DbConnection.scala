b23b9251c79596c70def576ed4ba7b2c588a899c
Andreas Bernauer <andreas.bernauer@active-group.de>
2013-07-30 13:16:02 +0200

    Document DbConnection

--- src/main/scala/de/ag/sqala/DbConnection.scala
+++ src/main/scala/de/ag/sqala/DbConnection.scala
@@ -3,9 +3,20 @@ package de.ag.sqala
 import de.ag.sqala.sql._
 import de.ag.sqala.relational.Schema
 
+/**
+ * Driver specific object that holds actual connection to database.
+ *
+ * Currently only JDBC is supported.
+ */
 sealed abstract class Handle
 case class JDBCHandle(connection: java.sql.Connection) extends Handle
 
+/**
+ * Iterator of the result of a database read operation.
+ *
+ * Basically wrap java.sql.ResultSet into an Scala iterator over rows.
+ * @param resultSet Underlying java.sql.ResultSet
+ */
 class ResultSetIterator(resultSet:java.sql.ResultSet) extends Iterator[Seq[AnyRef]] {
 
 private val metaData = resultSet.getMetaData
@@ -39,18 +50,54 @@ private val metaData = resultSet.getMetaData
 
 
 /**
- *
+ * (Open) Connection to a database via a specific driver
  */
 trait DbConnection {
-  val kind: Symbol       // what kind of database we're attached to
-  val name: String       // same, for humans
-  val handle: Handle     // DB-specific connection handle (?)
+  /** what kind of database we're attached to (choose freely but uniquely...) */
+  val kind: Symbol
+  /** what kind of database we're attached to (for humans; choose freely but uniquely...) */
+  val name: String
+  /** DB-specific connection handle */
+  val handle: Handle
+  /** parameterization of writing SQL strings */
   val sqlWriteParameterization: WriteParameterization
 
+  /** Close this connection. All subsequent methods calls are undefined. */
   def close():Unit
+  /** Read results from database described in table, expecting schema */
   def read(table:Table, schema:Schema): ResultSetIterator
+
+  /** Insert values into database
+    *
+    * @param table   Name of table to which to add values
+    * @param schema  Schema of the table that is being appended
+    * @param values  Values to add (single row)
+    * @return        Number of rows that have been added
+    */
   def insert(table:Table.TableName, schema:Schema, values:Seq[AnyRef]): Int
+
+  /** Delete values from database
+    *
+    * @param table      Name of table from which to delete values
+    * @param condition  Condition that each row that is to be deleted fulfills (use tautology to delete all rows)
+    * @return           Number of rows that have been deleted
+    */
   def delete(table:Table.TableName, condition:Expr): Int
-  def update(table:Table.TableName, condition:Expr, updates:Seq[(String, Expr)]): Int
+
+  /** Update values in database
+    *
+    * @param table      Name of table which is to be updated
+    * @param condition  Condition that each row that is to be updated fulfills (use tautology to update all rows)
+    * @param updates    Which column is to be updated to which value (expression)
+    * @return           Number of rows that have been updated
+    */
+  def update(table:Table.TableName, condition:Expr, updates:Seq[(Schema.Attribute, Expr)]): Int
+
+  /** Execute raw SQL string. For 'emergencies' and database- and driver-specific stuff.
+    *
+    * @param sql Raw SQL string that is sent to driver
+    * @return    Whatever driver returns for that SQL. For example, JDBC returns ResultSet for 'queries'
+    *            or Integer for 'updates'
+    */
   def execute(sql:String):Any // run raw , for emergencies
 }
