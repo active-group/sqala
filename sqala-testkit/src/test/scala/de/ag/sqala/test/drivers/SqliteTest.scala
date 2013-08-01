4516529cc9149a2f102c2e0cc853d0823812b306
Andreas Bernauer <andreas.bernauer@active-group.de>
2013-08-01 08:41:31 +0200

    Implement insertAndRetrieveGeneratedKeys; fix test

--- sqala-testkit/src/test/scala/de/ag/sqala/test/drivers/SqliteTest.scala
+++ sqala-testkit/src/test/scala/de/ag/sqala/test/drivers/SqliteTest.scala
@@ -134,9 +134,10 @@ class SqliteTest extends FunSuite with BeforeAndAfter {
   test("retrieve generated keys") {
     conn.createTable("tbl2", tbl2Schema)
     for(i <- 1 to 10) {
-      expectResult((1, i)) {
-          conn.insertAndRetrieveGeneratedKeys("tbl2", tbl2Schema, Seq(null))
-        }
+      expectResult((1, Seq(i))) {
+        val (rowCount, keys) = conn.insertAndRetrieveGeneratedKeys("tbl2", tbl2Schema, Seq(null))
+        (rowCount, keys.toSeq)
+      }
     }
   }
 }
