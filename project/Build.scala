27c42df334d0a40877d1161bfdf2336148b1dd26
Mike Sperber <sperber@deinprogramm.de>
2016-11-16 16:49:21 +0100

    Specify Scala 2.10.6.

--- project/Build.scala
+++ project/Build.scala
@@ -5,14 +5,14 @@ import Keys._
 object SqalaBuild extends Build {
   lazy val root =
     Project(id = "Sqala",
-            base = file("."),
-            settings = Defaults.coreDefaultSettings ++ Seq(
-              libraryDependencies ++= Seq(
-	        // needed for: minitest
-                "io.monix" %% "minitest" % "0.19" % "test"),
-              testFrameworks += new TestFramework("minitest.runner.Framework")
-            ))
-
+      base = file("."),
+      settings = Defaults.coreDefaultSettings ++ Seq(
+        scalaVersion := "2.10.6",
+        libraryDependencies ++= Seq(
+          // needed for: minitest
+          "io.monix" %% "minitest" % "0.19" % "test"),
+        testFrameworks += new TestFramework("minitest.runner.Framework")
+      ))
 }
 
 
